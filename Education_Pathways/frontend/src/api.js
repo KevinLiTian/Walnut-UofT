37183912bc32432e5b30ce4f85dd81fc21dd29bf
KevinLiTian <kevin.li20021106@gmail.com>
2022-11-26 02:37:30 -0500

    Change baseURL to render.com

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:5000/",
-  baseURL: "https://walnut-uoft.herokuapp.com/",
+  // baseURL: "https://walnut-uoft.herokuapp.com/",
+  baseURL: "https://walnut-uoft.onrender.com/",
 });
